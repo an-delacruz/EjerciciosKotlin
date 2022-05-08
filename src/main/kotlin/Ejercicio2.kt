666db8d56dcad1375f05bb60035278bd4fca6231
Antonio De la cruz <59275072+an-delacruz@users.noreply.github.com>
2022-05-08 11:04:55 -0500

    Update Ejercicio2.kt

--- src/main/kotlin/Ejercicio2.kt
+++ src/main/kotlin/Ejercicio2.kt
@@ -1,9 +1,5 @@
 import kotlin.math.floor
 
-/**
- * You can edit, run, and share this code.
- * play.kotlinlang.org
- */
 fun main() {
     //     println("Hello, world!!!")
     println("Datos persona 1")
